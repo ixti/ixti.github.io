fcab6ae4a5ab14cc0bbbc413f98b7c35c06c0941
Aleksey V Zapparov <ixti@member.fsf.org>
2013-01-28 13:38:20 +0100

    Improve excerpt plugin
    Use logic of pull-request: https://github.com/mojombo/jekyll/pull/727

--- _plugins/excerpt.rb
+++ _plugins/excerpt.rb
@@ -1,6 +1,7 @@
 # jekyll-excerpt
 #
 # Adds Post#excerpt that returns HTML of the first paragraph of a post.
+# Based on Jekyll pull-request: https://github.com/mojombo/jekyll/pull/727
 #
 # Copyright (C) 2012 Aleksey V Zapparov (http://ixti.net/)
 #
@@ -35,31 +36,73 @@ module Jekyll
           alias_method :to_liquid_without_excerpt, :to_liquid
 
           def to_liquid
-            to_liquid_without_excerpt.deep_merge({ 'excerpt' => excerpt })
+            to_liquid_without_excerpt.deep_merge({ 'excerpt' => self.excerpt })
           end
         end
       end
 
       module InstanceMethods
-        def excerpt
-          unless @excerpt
-            raw  = File.read(File.join(@base, @name))
 
-            # strip out YAML front-matter block
-            if raw =~ /^(---\s*\n.*?\n?)^(---\s*$\n?)/m
-              raw = $POSTMATCH
-            end
+        attr_accessor :excerpt
 
-            raw.strip!
 
-            head = raw.partition("\n\n").first
-            refs = raw.scan(/^\[[^\]]+\]:.+$/).join "\n"
-
-            @excerpt = converter.convert "#{head}\n\n#{refs}"
-          end
-
-          @excerpt
+        def read_yaml(*args)
+          super
+          self.excerpt = self.extract_excerpt
         end
+
+
+        def transform
+          super
+          self.excerpt = converter.convert(self.excerpt)
+        end
+
+
+        protected
+
+
+        # Internal: Extract excerpt from the content
+        #
+        # By default excerpt is your first paragraph of a post: everything
+        # before the first two new lines:
+        #
+        #     ---
+        #     title: Example
+        #     ---
+        #
+        #     First paragraph with [link][1].
+        #
+        #     Second paragraph.
+        #
+        #     [1]: http://example.com/
+        #
+        # This is fairly good option for Markdown and Textile files. But might
+        # cause problems for HTML posts (which is quiet unusual for Jekyll).
+        # If default excerpt delimiter is not good for you, you might want to
+        # set your own via configuration option `excerpt_separator`.
+        #
+        # For example, following is a good alternative for HTML posts:
+        #
+        #     # file: _config.yml
+        #     excerpt_separator: "<!-- more -->"
+        #
+        # Notice that all markdown-style link references will be appended to the
+        # excerpt. So the example post above will have this excerpt source:
+        #
+        #     First paragraph with [link][1].
+        #
+        #     [1]: http://example.com/
+        #
+        # Excerpts are rendered same time as content is rendered.
+        #
+        # Returns excerpt String
+        def extract_excerpt
+          separator     = self.site.config['excerpt_separator'] || "\n\n"
+          head, _, tail = self.content.partition(separator)
+
+          "" << head << "\n\n" << tail.scan(/^\[[^\]]+\]:.+$/).join("\n")
+        end
+
       end
     end
   end
